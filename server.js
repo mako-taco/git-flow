9a7b2d77b8cb8b6f88d1096d4ca27f2f5b3a40ae
mako-taco <jake.y.scott@gmail.com>
2014-02-13 11:11:17 -0500

    done

---
+++ server.js
@@ -0,0 +1,90 @@
+var express = require('express');
+var exec = require('child_process').exec;
+
+module.exports = function (opts) {
+	opts = opts || {};
+
+	if(opts.remote === undefined) {
+		throw new Error("Missing required paramater `remote`");
+	}
+	
+	//Required
+	var remote = opts.remote;
+
+	//Optional
+	var port = opts.port || 8888;
+	var route = opts.route || "/";
+	var local = opts.local || "./";
+	var user = opts.user || "";
+	var pass = opts.pass || "";
+
+	//Private
+	var refs = {};
+
+
+	this.app = express();	
+	if(user && pass) {
+		this.app.use(express.basicAuth({
+			user: user,
+			pass: pass
+		}))
+	}
+
+	this.app.post(route, requestHandler);
+	this.hook = hook;
+	this.pull = pull;
+	this.app.listen(port);
+
+	function requestHandler (req, res, next) {
+		res.send(200);
+		try {
+			var payload = JSON.parse(req.body.payload);
+			var fullRef = payload.ref;
+			var shortRef = fullRef.split('refs/heads/')[1];
+			var action = refs[fullRef] || refs[shortRef];
+
+			if(action) {
+				action(payload);
+			}
+		}
+		catch(e) {
+			console.error("Could not parse post-recieve hook body from githib:", req.body);
+			console.error(e.stack);
+		}
+
+	}.bind(this);
+
+	function hook (branch, action) {
+		if(refs[branch] !== undefined) {
+			console.warn("Overwriting hook for " + branch);
+		}
+		refs[branch] = action.bind(this);
+	}.bind(this);
+
+	function pull (branch, callback) {
+		var stdout = "", stderr = "";
+		var git = spawn("git", ["pull", "origin", branch, "--ff-only"], {
+			cwd: local
+		});
+
+		git.stderr.on("data", function (data) {
+			stderr += data.toString();
+		});
+
+		git.stdout.on("data", function (data) {
+			stdout += data.toString();
+		});
+
+		git.on("close", function(code) {
+			if (code != 0) {
+				callback(new Error("Git exited with status code " + code + ": " + stderr));
+			}
+			else {
+				callback(null, stdout);
+			}
+		});
+	}.bind(this)
+};
+
+
+
